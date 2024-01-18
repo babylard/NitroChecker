9132fcce0cfe819f7da8afe9d7cabcf8cf7e5f3f
babylard <babylard1@protonmail.com>
2024-01-18 12:51:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# NitroChecker
+
+An application I found on replit which came with a bunch of nitro links in a .txt file, along with a bunch of useless Print statements in the python file to make it look like it was doing something. I improved it a bit and im just uploading it to Github for friends to use .,.
+
+There are 10000 lines in total in the `links.txt` file, the application will check a certain amount of links you tell it to to see if they are working or not.
+
---
+++ main.py
@@ -0,0 +1,45 @@
+import time
+import random, string
+import webbrowser
+import requests
+
+blue="\33[0;34m"
+cyan="\033[1;36m"
+purple="\033[0;35m" 
+green="\033[0;32m"
+orange ="\033[0;33m"
+pink = "\033[1;31m"
+darkblue = "\033[1;34m"
+white = "\033[1;37m"
+gray = "\033[1;30m"
+red = "\033[0;31;47m"
+
+num=input('Input how many links you would like to check: ')
+print("---------------------------------------------------------------")
+
+f=open("links.txt","w", encoding='utf-8')
+      
+for n in range(int(num)):
+   y = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
+   f.write('https://discord.gift/')
+   f.write(y)
+   f.write("\n")
+
+f.close()
+
+
+with open("links.txt") as f:
+    for line in f:
+        nitro = line.strip("\n")
+
+        url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+
+        r = requests.get(url)
+
+        if r.status_code == 200:
+            print(green," Working | {} ".format(line.strip("\n")))
+            break
+        else:
+        	print(darkblue," Expired/Used | {} ".format(line.strip("\n")))
+
+input("Done. Press Enter to exit.")
